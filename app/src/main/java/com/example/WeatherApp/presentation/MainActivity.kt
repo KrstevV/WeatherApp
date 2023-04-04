a02d283348f0a73a2d85af800337bb3f03c71d71
Viktor Krstev <krstevviktor996@gmail.com>
2023-04-04 15:26:34 +0200

    Code

--- app/src/main/java/com/example/WeatherApp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/WeatherApp/presentation/MainActivity.kt
@@ -17,12 +17,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     lateinit var AdapterR : Adapter
 
-    var asdas : String = "sdfsdad"
-    var sdfsdfs : String = "sdfqwe"
-    var sgfdsf : Int = 123
-    var sdfsfs : String = "qweqq"
-    var sdfdsfs: String = "sdfsfsfs"
-    var sdfdssdfs : Int = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
